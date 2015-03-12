19b68b1a6717907e4b845d390b38675c4ed022eb
Suteepat Damrongyingsupab <suteepat@maqe.com>
2015-03-12 19:25:59 +0700

    Added get attributes function and fixed code to PSR-2.

--- src/AwsHelper/SqsHelper.php
+++ src/AwsHelper/SqsHelper.php
@@ -5,10 +5,19 @@ use Aws\S3\Exception\S3Exception;
 
 class SqsHelper
 {
+
+    protected $sqs;
+    protected $queue_url;
+
     public function __construct(AwsHelper $adapter, $queue_url)
     {
-        $this->queue_url = $queue_url;
+        $this->setQueueUrl($queue_url);
         $this->sqs = SqsClient::factory($adapter->getDefaultOptions());
+    }
+
+    public function setQueueUrl($queue_url)
+    {
+        $this->queue_url = $queue_url;
     }
 
     /* For when you want to stop the listener from running */
@@ -20,11 +29,10 @@ class SqsHelper
     /* Begin listening for messages from the SQS queue.
     @param $num_msg Dictates how many messages you wish to process at a time
     */
-    public function listen($num_msg=1)
+    public function listen($num_msg = 1)
     {
         $this->stop = false;
-        while(!$this->stop)
-        {
+        while (!$this->stop) {
             $messages = $this->sqs->receiveMessage([
                 'QueueUrl'        => $this->queue_url,
 
@@ -33,11 +41,11 @@ class SqsHelper
                 'MaxNumberOfMessages' => $num_msg,
             ]);
 
-            if (!$messages->hasKey('Messages'))
+            if (!$messages->hasKey('Messages')) {
                 continue;
+            }
 
-            foreach($messages->get('Messages') as $msg_data)
-            {
+            foreach ($messages->get('Messages') as $msg_data) {
                 yield new SqsMessage($msg_data);
             }
         }
@@ -59,7 +67,7 @@ class SqsHelper
     @params $data The data of your message.
     @params $attributes Any optional meta data attributes you wish to feed through
     */
-    public function push($data, $attributes=[])
+    public function push($data, $attributes = [])
     {
         return $this->sqs->sendMessage([
             'QueueUrl' => $this->queue_url,
@@ -67,10 +75,38 @@ class SqsHelper
             'MessageAttributes' => $attributes,
         ]);
     }
+
+    /**
+     * Get a list of queue attributes
+     * @param  array $attributes An array of queue attribute names
+     * @return array             An array of specified queue attributes with keys as attribute names
+     */
+    public function getQueueAttributes($attributes)
+    {
+        $queueAttributes = $this->sqs->getQueueAttributes([
+            'QueueUrl' => $this->queue_url,
+            'AttributeNames' => $attributes,
+        ]);
+        if (!$queueAttributes->hasKey('Attributes')) {
+            return [];
+        }
+        return $queueAttributes->get('Attributes');
+    }
+
+    /**
+     * Shorthand function to get a single queue attribute
+     * @param  string $attribute A Queue attribute
+     * @return mixed             String data of the specified queue attribute or null
+     */
+    public function getQueueAttribute($attribute)
+    {
+        $queueAttributes = $this->getQueueAttributes([$attribute]);
+        return array_key_exists($attribute, $queueAttributes) ? $queueAttributes[$attribute] : null;
+    }
 }
 
 
-Class SqsMessage
+class SqsMessage
 {
     public function __construct($msg_data)
     {
